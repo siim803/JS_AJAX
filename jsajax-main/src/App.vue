5e331be119a0001140bbd266bf8e32cf7328611a
siim803 <siimuutma7@gmail.com>
2026-03-25 13:10:50 +0200

    Getting up to speed

--- jsajax-main/src/App.vue
+++ jsajax-main/src/App.vue
@@ -1,18 +1,17 @@
 <script setup>
-import { ref } from 'vue';
+    import { ref } from 'vue';
 
-let message = ref('Hello Vue!')
+    let message = ref('I need help'); //Ref updateib kõike, mis kasutab seda kui see muutub
+
 
 </script>
 
 <template>
     <div class="container">
         <h1>{{ message.split('').reverse().join('') }}</h1>
-        <button class="button is-primary" @click="message = 'Hello TA24A!'">Click me!</button>
+        <button class="button is-primary" @click="message = 'I need sleep!'">This is a button</button>
         <input class="input" v-model="message">
     </div>
 </template>
 
 <style>
-
-</style>
