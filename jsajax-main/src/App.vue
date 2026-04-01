89eabc2c5cfb67065fb8f8cebe6629e5696bc43f
siim803 <siim.uutma@tptlive.ee>
2026-04-01 13:13:58 +0300

    Shoping list

--- jsajax-main/src/App.vue
+++ jsajax-main/src/App.vue
@@ -1,16 +1,39 @@
 <script setup>
     import { ref } from 'vue';
 
-    let message = ref('I need help'); //Ref updateib kõike, mis kasutab seda kui see muutub
+    let items = ref(['Sai', 'Piim', 'Viin', 'Ma', 'Tahan', 'Magada'])
+    let new_item = ref('')
 
-
+    function add_shi()
+    {
+        if (new_item.value.trim())
+        {
+            items.value.push(new_item.value);
+            new_item.value = ''
+        }
+    }
 </script>
 
 <template>
     <div class="container">
-        <h1>{{ message.split('').reverse().join('') }}</h1>
-        <button class="button is-primary" @click="message = 'I need sleep!'">This is a button</button>
-        <input class="input" v-model="message">
+        <div class="field has-addons mt-3">
+            <div class="control is-expanded">
+                <input class="input" type="text" v-model="new_item" @keypress.enter="add_shi">
+            </div>
+            <div class="control">
+                <button class="button is-info" @click="add_shi">
+                    Add shit
+                </button>
+            </div>
+        </div>
+
+        <div class="content">
+            <h1>All items</h1>
+            <ul>
+                <li v-for="item in items">{{ item }}</li>
+            </ul>
+        </div>
+
     </div>
 </template>
 
